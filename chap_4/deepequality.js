054b2356d9fff11ee7ca9f8bbd7abfe40c13e07a
Abhishek Mukherjee <abhishekmukherjee@AbhishekM-MBP.local>
2021-02-11 22:16:20 +0530

    comments for clarity

--- chap_4/deepequality.js
+++ chap_4/deepequality.js
@@ -1,11 +1,13 @@
 const deepEqual = (x, y) => {
+  // comparing values
   if (x === y) return true;
-
+  // checking for objects
+  // checking for null as typeof null == 'object'!!!
   if (x == null || y == null || typeof x != "object" || typeof y != "object")
     return false;
-
+  // list of keys have different length
   if (Object.keys(x).length != Object.keys(y).length) return false;
-
+  // list of keys have same length
   for (let k of Object.keys(x)) {
     if (!Object.keys(y).includes(k) || !deepEqual(x[k], y[k])) return false;
   }
